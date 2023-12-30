dd2e25c64f0d3b30f18737c38ac9ae9cc0c3c7ce
Julio Geldres <geldresj@gmail.com>
2023-12-29 22:18:32 -0500

    FeedList_End

--- App.tsx
+++ App.tsx
@@ -1,41 +1,17 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import React from 'react';
-import colors from './src/theme/colors';
 import fonts from './src/theme/fonts';
 import FeedPost from './src/components/FeedPost/FeedPost';
-
-const post = {
-  id: '1',
-  createdAt: '19 December 2021',
-  image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
-  description:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic repellendus unde blanditiis. Eos fugiat dolorem ea fugit aut sapiente corrupti autem dolores deleniti architecto, omnis, amet unde dignissimos quam minima?',
-  user: {
-    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
-    username: 'vadimnotjustdev',
-  },
-  nofComments: 11,
-  nofLikes: 33,
-  comments: [
-    {
-      id: '1',
-      comment: 'Hello there',
-      user: {
-        username: 'vadimnotjustdev',
-      },
-    },
-    {
-      id: '2',
-      comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. H',
-      user: {
-        username: 'vadimnotjustdev',
-      },
-    },
-  ],
-};
-
+import posts from './src/assets/data/posts.json';
 const App = () => {
-  return <FeedPost post={post} />;
+  return (
+    <View>
+      <FlatList
+        data={posts}
+        renderItem={({item}) => <FeedPost post={item} />}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
