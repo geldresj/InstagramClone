b5b2b50063593cf15667d10151b6b200466e33b7
Julio Geldres <geldresj@gmail.com>
2024-01-12 13:15:33 -0500

    PostUploadScreen_end_only_for_return

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ProfileScreen from './src/screens/ProfileScreen/ProfileScreen';
 import EditProfileScreen from './src/screens/EditProfileScreen/EditProfileScreen';
 import PostUploadScreen from './src/screens/PostUploadScreen/PostUploadScreen';
 const App = () => {
